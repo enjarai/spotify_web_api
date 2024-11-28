dbc05a5896be92d7a15b3b257091176a9ea5ab64
Ricky Severino <rickyseverino@protonmail.com>
2024-11-28 13:40:06 -0500

    add get several albums endpoint

--- src/api/albums.rs
+++ src/api/albums.rs
@@ -1,3 +1,5 @@
 mod get_album;
+mod get_several_albums;
 
 pub use get_album::*;
+pub use get_several_albums::*;
---
+++ src/api/albums/get_several_albums.rs
@@ -0,0 +1,84 @@
+use crate::api::{Endpoint, QueryParams};
+use derive_builder::Builder;
+use http::Method;
+use std::borrow::Cow;
+
+/// Get Spotify catalog information for a single album.
+#[derive(Debug, Builder, Clone)]
+pub struct GetSeveralAlbums {
+    /// A list of the [Spotify ID](https://developer.spotify.com/documentation/web-api/concepts/spotify-uris-ids) for the albums.
+    ids: Vec<String>,
+
+    /// An [ISO 3166-1 alpha-2 country code](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2).
+    /// If a country code is specified, only content that is available in that market will be returned.
+    /// If a valid user access token is specified in the request header, the country associated with the user account will take priority over this parameter.
+    ///
+    /// # Notes
+    /// If neither market or user country are provided, the content is considered unavailable for the client.
+    /// Users can view the country that is associated with their account in the [account settings](https://www.spotify.com/account/overview/).
+    #[builder(setter(into, strip_option), default)]
+    market: Option<String>,
+}
+
+#[allow(dead_code)]
+impl GetSeveralAlbumsBuilder {
+    fn id(&mut self, id: impl Into<String>) -> &mut Self {
+        self.ids.get_or_insert_with(Vec::new).push(id.into());
+        self
+    }
+}
+
+impl GetSeveralAlbums {
+    pub fn builder() -> GetSeveralAlbumsBuilder {
+        GetSeveralAlbumsBuilder::default()
+    }
+}
+
+impl Endpoint for GetSeveralAlbums {
+    fn method(&self) -> Method {
+        Method::GET
+    }
+
+    fn endpoint(&self) -> Cow<'static, str> {
+        "albums".into()
+    }
+
+    fn parameters(&self) -> QueryParams<'_> {
+        let mut params = QueryParams::default();
+        params.push("ids", &self.ids.join(","));
+        params.push_opt("market", self.market.as_deref());
+        params
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        api::{self, Query as _},
+        test::client::{ExpectedUrl, SingleTestClient},
+    };
+
+    #[test]
+    fn endpoint_query() {
+        let endpoint = ExpectedUrl::builder()
+            .endpoint("albums")
+            .add_query_params(&[(
+                "ids",
+                "382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc",
+            )])
+            .build()
+            .unwrap();
+
+        let client = SingleTestClient::new_raw(endpoint, "");
+
+        let endpoint = GetSeveralAlbums::builder()
+            .id("382ObEPsp2rxGrnsizN5TX")
+            .id("1A2GTWGtFfWp7KSQTwWOyo")
+            .id("2noRn2Aes5aoNVsU6iWThc")
+            .build()
+            .unwrap();
+
+        api::ignore(endpoint).query(&client).unwrap();
+    }
+}
