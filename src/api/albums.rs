5525bd413963e8af3bb912c51d420a70f548d4fc
Ricky Severino <rickyseverino@protonmail.com>
2024-12-03 19:14:31 -0500

    add get album tracks endpoint

--- src/api/albums.rs
+++ src/api/albums.rs
@@ -1,7 +1,9 @@
 mod check_user_saved_albums;
 mod get_album;
+mod get_album_tracks;
 mod get_several_albums;
 
 pub use check_user_saved_albums::*;
 pub use get_album::*;
+pub use get_album_tracks::*;
 pub use get_several_albums::*;
