cb365c08239e0d83f213c50f79771d274ce83609
Ricky Severino <rickyseverino@protonmail.com>
2024-12-06 10:51:01 -0500

    add get user saved albums endpoint

--- src/api/albums.rs
+++ src/api/albums.rs
@@ -2,8 +2,10 @@ mod check_user_saved_albums;
 mod get_album;
 mod get_album_tracks;
 mod get_several_albums;
+mod get_user_saved_albums;
 
 pub use check_user_saved_albums::*;
 pub use get_album::*;
 pub use get_album_tracks::*;
 pub use get_several_albums::*;
+pub use get_user_saved_albums::*;
---
+++ src/api/albums/get_user_saved_albums.rs
@@ -0,0 +1,39 @@
+use crate::api::prelude::*;
+
+/// Get a list of the albums saved in the current Spotify user's 'Your Music' library.
+#[derive(Debug, Builder, Clone)]
+pub struct GetUserSavedAlbums {
+    /// An [ISO 3166-1 alpha-2 country code](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2).
+    /// If a country code is specified, only content that is available in that market will be returned.
+    /// If a valid user access token is specified in the request header, the country associated with the user account will take priority over this parameter.
+    ///
+    /// # Notes
+    /// If neither market or user country are provided, the content is considered unavailable for the client.
+    /// Users can view the country that is associated with their account in the [account settings](https://www.spotify.com/account/overview/).
+    #[builder(setter(into, strip_option), default)]
+    market: Option<String>,
+}
+
+impl GetUserSavedAlbums {
+    pub fn builder() -> GetUserSavedAlbumsBuilder {
+        GetUserSavedAlbumsBuilder::default()
+    }
+}
+
+impl Pageable for GetUserSavedAlbums {}
+
+impl Endpoint for GetUserSavedAlbums {
+    fn method(&self) -> Method {
+        Method::GET
+    }
+
+    fn endpoint(&self) -> Cow<'static, str> {
+        "me/albums".into()
+    }
+
+    fn parameters(&self) -> QueryParams<'_> {
+        let mut params = QueryParams::default();
+        params.push_opt("market", self.market.as_deref());
+        params
+    }
+}
